f2e145be9bfaa9facf4b7cf0a14c834ad29a1288
SnakeGameTeamProject <axinja.pro@yandex.ru>
2017-12-17 23:10:22 +0300

    Music addition

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
